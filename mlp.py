559620006bb16a218cfe526b1e6dab4cd3ed35e9
Rachidmerabet77 <rachidmerabet77@gmail.com>
2025-03-22 21:10:01 +0100

    Créer la classe MLP et initialiser les W et le B

--- mlp.py
+++ mlp.py
@@ -1,10 +1,8 @@
 import numpy as np
-
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 def relu(x):
     return np.maximum(0, x)
-
 class simplemlp:
     def __init__(self, NmbrNeuronesEntrer, NmbrCouchesCachees, NmbrNeuronesSortie):
         self.couches = [NmbrNeuronesEntrer] + NmbrCouchesCachees + [NmbrNeuronesSortie]
@@ -13,3 +11,5 @@ class simplemlp:
         for i in range(len(self.couches) - 1):
             self.W.append(np.random.randn(self.couches[i], self.couches[i+1]) * 0.01)
             self.B.append(np.zeros((1, self.couches[i+1])))
+
+   
