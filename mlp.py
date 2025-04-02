ce118eab3a7d7e90d26c556f8527b6c4de0a14a0
Rachidmerabet77 <rachidmerabet77@gmail.com>
2025-04-02 03:56:01 +0100

    Créer la classe MLP et initialiser W et B

--- mlp.py
+++ mlp.py
@@ -12,4 +12,20 @@ class simplemlp:
             self.W.append(np.random.randn(self.couches[i], self.couches[i+1]) * 0.01)
             self.B.append(np.zeros((1, self.couches[i+1])))
 
-   
+    def Avantpropa(self, X):
+        A = X
+        for i in range(len(self.W)):
+            Z = np.dot(A, self.W[i]) + self.B[i]
+            if i < len(self.W) - 1:
+                A = relu(Z) 
+            else:
+                A = sigmoid(Z) 
+            print("iteration ", i, ":")
+            print(A)
+        return A
+    
+mlp = simplemlp(NmbrNeuronesEntrer=4, NmbrCouchesCachees=[10, 5], NmbrNeuronesSortie=1)
+test = np.array([[0.2, 0.5, 0.8, 0.7]]) 
+ 
+Z = mlp.Avantpropa(test)
+print("Sortie du resau de neurone:", Z)
