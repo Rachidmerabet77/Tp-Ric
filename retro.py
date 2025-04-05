31997e766f8536ee2af27bd75a0a461440193c25
Rachidmerabet77 <rachidmerabet77@gmail.com>
2025-04-05 02:29:59 +0100

    tester l'exmple de XOR (success)

--- retro.py
+++ retro.py
@@ -4,10 +4,10 @@ def sigmoid(x):
 def derivee_sigmoid(x):
     return x * (1 - x)
 
-#def relu(x):
- #   return np.maximum(0, x)
-#def derivee_relu(x):
- #   return (x > 0).astype(float)
+def relu(x):
+   return np.maximum(0, x)
+def derivee_relu(x):
+   return (x > 0).astype(float)
 
 class simplemlp:
     def __init__(self, nb_neurones_entree, couches_cachees, nb_neurones_sortie, taux_apprentissage=0.01):
@@ -26,9 +26,9 @@ class simplemlp:
             Z = np.dot(activations[-1], self.poids[i]) + self.biais[i]
             valeurs_Z.append(Z)
             if i < len(self.poids) - 1:
-                A = sigmoid(Z)  # Activation ReLU pour les couches cachées
+                A = relu(Z)
             else:
-                A = sigmoid(Z)  # Activation sigmoïde pour la couche de sortie
+                A = sigmoid(Z)
             activations.append(A)
             
              
@@ -46,7 +46,10 @@ class simplemlp:
            #  calculer les gradients des Wet de B pour chaque couche   
         for i in reversed(range(len(self.poids))):
            
-            dZ = dA * derivee_sigmoid(valeurs_Z[i])  #calculer la deriveer pour  tout les couvhes cachees
+            if i == len(self.poids) - 1:
+                dZ = dA * derivee_sigmoid(activations[i+1])#calculer la deriveer pour  la cachee de sortie
+            else:
+                dZ = dA * derivee_relu(valeurs_Z[i])  #calculer la deriveer pour  les couvhes cachees
         
             dW[i] = np.dot(activations[i].T, dZ) / m  # Gradient des valeur de W
             dB[i] = np.sum(dZ, axis=0, keepdims=True) / m  # Gradient  de la valeur de B
@@ -59,13 +62,12 @@ class simplemlp:
             self.biais[i] -= self.taux_apprentissage * dB[i]
             
 # l'entrainement de mlp
-    def entrainer(self, X, Y, epochs=1000):
+    def entrainer(self, X, Y, epochs=10000):
         for epoch in range(epochs):
             self.retropropagation(X, Y)
-            if epoch % 100 == 0:
+            if epoch % 1000 == 0:
                 activations, _ = self.propagation_avant(X)
                 erreur = np.mean((Y - activations[-1])**2)
-                #print(f"Epoch {epoch}, Erreur: {erreur:.5f}")
         return erreur        
 
 #exmple
@@ -73,8 +75,8 @@ X_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 Y_train = np.array([[0], [1], [1], [0]])
 
 
-mlp = simplemlp(nb_neurones_entree=2, couches_cachees=[4,2], nb_neurones_sortie=1, taux_apprentissage=0.05)
-Resultat = mlp.entrainer(X_train, Y_train, epochs=1000)
+mlp = simplemlp(nb_neurones_entree=2, couches_cachees=[4], nb_neurones_sortie=1, taux_apprentissage=1)
+Resultat = mlp.entrainer(X_train, Y_train, epochs=10000)
 print("errore de entrenments :", Resultat)
 
 activations, _ = mlp.propagation_avant(X_train)
